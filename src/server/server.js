08eec19a4291637f24adc132a1955f644169a24b
VFS <pg08alex@vfs.com>
2017-06-06 20:54:03 -0700

    add color to the spider

--- src/server/server.js
+++ src/server/server.js
@@ -45,6 +45,8 @@ var pool = sql.createConnection({
     database: s.database
 });
 
+var spiderColorsHue = [0, 10, 30, 50, 70, 100, 190, 210, 240, 270, 300, 320];
+
 //log sql errors
 pool.connect(function (err) {
     if (err) {
@@ -95,7 +97,7 @@ io.on('connection', function (socket) {
         cells: [],
         massTotal: c.defaultPlayerMass,
         webAttach : null,
-        hue: Math.round(Math.random() * 360),
+        hue : spiderColorsHue[Math.round(Math.random() * spiderColorsHue.length)],
         lastHeartbeat: new Date().getTime(),
         target: {
             x: 0,
@@ -132,8 +134,6 @@ io.on('connection', function (socket) {
             }];
             player.massTotal = c.defaultPlayerMass;
 
-
-            player.hue = Math.round(Math.random() * 360);
             currentPlayer = player;
             currentPlayer.lastHeartbeat = new Date().getTime();
             users.push(currentPlayer);
@@ -706,7 +706,7 @@ function sendUpdates() {
                                 cells: f.cells,
                                 webAttach:f.webAttach,
                                 massTotal: Math.round(f.massTotal),
-                                hue: f.hue,
+                                hue: f.hue
                             };
                         }
                     }
